0a459fde4de4abe4e4ec6f6c558598ae2381a096
Madhava Dama <dama.madhava.v2.0@gmai.com>
2024-07-26 17:48:30 +0530

    fixed AB#1

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
 
     <h1>Gen Ai for Every Business Custom</h1>
-	<h2>Upload your data to get Gen Ai predictions</h2>
+	<h2>Upload your data to get predictions from </h2>
     <img
       alt="Vue logo"
       style="width: 100px; height: 100px"
